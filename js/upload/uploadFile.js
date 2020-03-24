55b50fb9aaf41b5b11565c87e0e8bfd9fd67dee7
dlnet <ben.ray.com@gmail.com>
2020-03-23 20:44:22 -0600

    Update uploadFile.js

--- js/upload/uploadFile.js
+++ js/upload/uploadFile.js
@@ -1,17 +1,3 @@
-  // Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyB184WtbZKmG5ZYl4ll2ZOY4tBoncFEx_k",
-    authDomain: "seng300-48c52.firebaseapp.com",
-    databaseURL: "https://seng300-48c52.firebaseio.com",
-    projectId: "seng300-48c52",
-    storageBucket: "seng300-48c52.appspot.com",
-    messagingSenderId: "674151113181",
-    appId: "1:674151113181:web:8ceac73abaaf0d78aa9634",
-    measurementId: "G-H2LN7DGHF1"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  console.log(firebase);
 
   function submit(){
     const ref = firebase.storage().ref('/studentDocuments')
