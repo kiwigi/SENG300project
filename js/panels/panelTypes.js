9e756611e9f33d2b62dcaae9a65e46b7780cfed5
Ben Raymond <ben.ray.com@gmail.com>
2020-03-30 23:48:44 -0600

    Implemented index panel functionality

---
+++ js/panels/panelTypes.js
@@ -0,0 +1,97 @@
+const panelTypes = {
+  "Admin" : {
+    "numPanels" : 3,
+    "panels" : {
+      0 : {
+        "href" : "scholarshipPanel.html",
+        "h3" : "Scholarship Panel",
+        "ic-class" : "fa fa-graduation-cap fa-5x",
+        "p" : "Add new scholarships to the database."
+      },
+      1 : {
+        "href" : "https://console.firebase.google.com/project/seng300-48c52/overview",
+        "h3" : "Firebase Console",
+        "ic-class" : "fa fa-database fa-5x",
+        "p" : "Go to the Firebase console."
+      },
+      2 : {
+        "href" : "https://github.com/kiwigi/SENG300project",
+        "h3" : "Code Repository",
+        "ic-class" : "fa fa-github fa-5x",
+        "p" : "View the code repository on Github."
+      }
+    }
+  },
+
+  "Student" : {
+    "numPanels" : 3,
+    "panels" : {
+      0 : {
+        "href" : "scholarships.html",
+        "h3" : "Apply Now",
+        "ic-class" : "fa fa-clipboard fa-5x",
+        "p" : "Apply to various scholarships."
+      },
+      1 : {
+        "href" : "index.html",
+        "h3" : "My Applications",
+        "ic-class" : "fa fa-file fa-5x",
+        "p" : "View your current scholarship applications."
+      },
+      2 : {
+        "href" : "account.html",
+        "h3" : "Student Information",
+        "ic-class" : "fa fa-user fa-5x",
+        "p" : "View your degree type, GPA, etc."
+      }
+    }
+  },
+
+  "Professor" : {
+    "numPanels" : 3,
+    "panels" : {
+      0 : {
+        "href" : "scholarships.html",
+        "h3" : "View Scholarships",
+        "ic-class" : "fa fa-clipboard fa-5x",
+        "p" : "View current available scholarships."
+      },
+      1 : {
+        "href" : "index.html",
+        "h3" : "Nominate Students",
+        "ic-class" : "fa fa-star fa-5x",
+        "p" : "Nominate students for a scholarship."
+      },
+      2 : {
+        "href" : "account.html",
+        "h3" : "Account Information",
+        "ic-class" : "fa fa-user fa-5x",
+        "p" : "View your account information."
+      }
+    }
+  },
+
+  "Coordinator" : {
+    "numPanels" : 3,
+    "panels" : {
+      0 : {
+        "href" : "scholarships.html",
+        "h3" : "View Scholarships",
+        "ic-class" : "fa fa-clipboard fa-5x",
+        "p" : "View current available scholarships."
+      },
+      1 : {
+        "href" : "index.html",
+        "h3" : "Review Applications",
+        "ic-class" : "fa fa-list fa-5x",
+        "p" : "Review current pending scholarship applications."
+      },
+      2 : {
+        "href" : "account.html",
+        "h3" : "Account Information",
+        "ic-class" : "fa fa-user fa-5x",
+        "p" : "View your account information."
+      }
+    }
+  }
+}
