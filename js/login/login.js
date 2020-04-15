38b6709c788718441a11042f6e8f39cd67794e93
gabpili <47364598+gabpili@users.noreply.github.com>
2020-04-14 21:23:40 -0600

    Update login.js

--- js/login/login.js
+++ js/login/login.js
@@ -20,43 +20,6 @@
     .catch(e => console.log("> ERROR: ",e.message));
   });
 
-  /*
-
-  Signing up a user (disabled for now)
-
-  // Add signup event
-  btnSignUp.addEventListener('click', e => {
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-    // Sign up
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise
-      .then(credential =>{
-        console.log('uid just created',credential.user.uid);
-        return firebase.database().ref("Users/Student/"+credential.user.uid).set({
-          name: "Test",
-          email: credential.user.email,
-          year: 2020,
-          degree: {
-            type: "BSc",
-            major: "CPSC",
-            minor: "MATH"
-          },
-          graduateType: "UNDERGRAD",
-          department: "Faculty of Science",
-          gpa: 4.0,
-          scholarshipsAppliedFor: [],
-          scholarShipsAwarded: [],
-          scholarshipAccepted: null,
-          dateCreated: Date(),
-          addedBy: null
-        })
-      })
-      .catch(e => console.log(e.message));
-  });
-  */
-
   // Add a realtime listener
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser) {
